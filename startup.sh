57dbd05d56d2aa2412c1e8d7b5b912189e435170
ZhouYang <zhouyang.me@gmail.com>
2015-03-01 23:23:24 +0800

    fix startup.sh port

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-nohup python homegate.py --conf=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18000 > 18000.out 2>&1 &
-nohup python homegate.py --conf=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18001 > 18001.out 2>&1 &
-nohup python homegate.py --conf=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18002 > 18002.out 2>&1 &
-nohup python homegate.py --conf=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18003 > 18003.out 2>&1 &
+nohup python homegate.py --config=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18000 > 18000.out 2>&1 &
+nohup python homegate.py --config=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18001 > 18001.out 2>&1 &
+nohup python homegate.py --config=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18002 > 18002.out 2>&1 &
+nohup python homegate.py --config=/usr/webapps/python/homegate/config/teldrassil.yaml --port=18003 > 18003.out 2>&1 &
